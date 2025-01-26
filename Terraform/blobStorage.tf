6f41e10ae2e715e0723adb73fba1562960937f84
nic5694 <nicholasmartoccia04@icloud.com>
2025-01-25 21:05:56 -0500

    added cosmos db and blob storage for the post form endpoint

--- Terraform/blobStorage.tf
+++ Terraform/blobStorage.tf
@@ -8,6 +8,6 @@ resource "azurerm_storage_account" "blobStorageAccount" {
 resource "azurerm_storage_container" "blobStorageContainer" {
   name                  = "user-pdf-submission"
   storage_account_id    = azurerm_storage_account.blobStorageAccount.id
-  container_access_type = "private"
+  container_access_type = "container"
 }
 
--- app.py
+++ app.py
@@ -1,16 +1,74 @@
-from flask import Flask
+from flask import Flask, request
+from flask import redirect, render_template, send_from_directory, url_for
+import os
+from werkzeug.utils import secure_filename
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.cosmos import CosmosClient, PartitionKey
+import random
+from dotenv import load_dotenv
+import string, random, requests
 
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
-
+load_dotenv() 
+account = os.getenv('ACCOUNT')
+key = os.getenv('KEY')
+container = os.getenv('CONTAINER')
+cosmos_endpoint = os.getenv('COSMOS_ENDPOINT')
+cosmos_key = os.getenv('COSMOS_KEY')
+cosmos_database_name = os.getenv('COSMOS_DATABASE_NAME')
+cosmos_container_name = os.getenv('COSMOS_CONTAINER_NAME')
+storage_connection_string = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 app = Flask(__name__)
+
+# Initialize the BlobServiceClient
+client_blob_service_client = BlobServiceClient.from_connection_string(storage_connection_string)
+container_blob = client_blob_service_client.get_container_client(container)
+
+# Initialize the CosmosClient
+cosmos_client = CosmosClient(cosmos_endpoint, cosmos_key)
+cosmos_database = cosmos_client.get_database_client(cosmos_database_name)
+cosmos_container = cosmos_database.get_container_client(cosmos_container_name)
 
 
 @app.route('/healthz')
 def health_check():
     return 'OK', 200
 
+@app.route('/form', methods=['POST'])
+def form():
+    file = request.files['cv']
+    filename = secure_filename(file.filename)
+    file_extension = filename.rsplit('.', 1)[1]
+    random_filename = id_generator()
+    filename = random_filename + '.' + file_extension
+    try:
+        blob_client_test = container_blob.get_blob_client(filename)
+        blob_client_test.upload_blob(data=file, overwrite=True)
+    except Exception as e:
+        print(f'Exception={e}')
+        pass
+    ref = f'https://{account}.blob.core.windows.net/{container}/{filename}'
+
+    name = request.form.get('name')
+    job_title = request.form.get('jobTitle')
+    tolerance = request.form.get('tolerance')
+    entity = {
+        'id': random_filename,
+        'name': name,
+        'jobTitle': job_title,
+        'tolerance': tolerance,
+        'blobStorageRef': ref,
+        'feedbackText': '',
+        'isRead': False
+    }
+    try:
+        cosmos_container.create_item(body=entity)
+    except Exception as e:
+        print(f'Exception={e}')
+        pass
+    return 'Created',201
+
+def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
