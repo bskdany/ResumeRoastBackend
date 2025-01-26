a048e8fa3dda9afec39fe7910f040f3659fc153e
nic5694 <nicholasmartoccia04@icloud.com>
2025-01-25 23:21:26 -0500

    added startup.sh to start the server proprely

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create virtual environment
+python -m venv /home/site/wwwroot/antenv
+
+# Activate virtual environment
+source /home/site/wwwroot/antenv/bin/activate
+
+# Install dependencies
+pip install -r /home/site/wwwroot/requirements.txt
+
+# Run your app
+gunicorn --bind 0.0.0.0:8080 your_app:app
